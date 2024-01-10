4c8f4a22ab5927907a2a7889c5dbb6440b43e1da
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-10 09:25:46 +0100

    add: layout with bootstrap (complete)

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ require_once __DIR__ . "/database/db.php";
 
 
 <body>
-  <div class="container mt-5">
+  <div class="container mt-5 mb-5">
   <h2>E-COMMERCE</h2>
   <!-- CARD -->
-  <div class="row d-flex">
+  <div class="row d-flex gy-3">
 
   <div class="col-4">
   <div class="card">
@@ -40,6 +40,60 @@ require_once __DIR__ . "/database/db.php";
         <li class="list-group-item">Animal: <?php echo $dry_food2->getCategory()->get_animal() ?></li>
         <li class="list-group-item">Price: <?php echo $dry_food2->getPrice() ?></li>
         <li class="list-group-item">Weight: <?php echo $dry_food2->getWeight() ?></li>
+        
+      </ul>
+    </p>
+    <a href="#" class="btn btn-primary">Buy</a>
+  </div>
+</div>
+</div>
+
+  <div class="col-4">
+  <div class="card">
+  <img src="<?php echo $toy1->getImage() ?>" class="card-img-top w-75 p-2" alt="">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $toy1->getName() ?></h5>
+    <p class="card-text">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Animal: <?php echo $toy1->getCategory()->get_animal() ?></li>
+        <li class="list-group-item">Price: <?php echo $toy1->getPrice() ?></li>
+        <li class="list-group-item">Material: <?php echo $toy1->getMaterial() ?></li>
+        
+      </ul>
+    </p>
+    <a href="#" class="btn btn-primary">Buy</a>
+  </div>
+</div>
+</div>
+
+  <div class="col-4">
+  <div class="card">
+  <img src="<?php echo $toy2->getImage() ?>" class="card-img-top w-75 p-2" alt="">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $toy2->getName() ?></h5>
+    <p class="card-text">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Animal: <?php echo $toy2->getCategory()->get_animal() ?></li>
+        <li class="list-group-item">Price: <?php echo $toy2->getPrice() ?></li>
+        <li class="list-group-item">Material: <?php echo $toy2->getMaterial() ?></li>
+        
+      </ul>
+    </p>
+    <a href="#" class="btn btn-primary">Buy</a>
+  </div>
+</div>
+</div>
+
+  <div class="col-4">
+  <div class="card">
+  <img src="<?php echo $kennel->getImage() ?>" class="card-img-top w-75 p-2" alt="">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $kennel->getName() ?></h5>
+    <p class="card-text">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Animal: <?php echo $kennel->getCategory()->get_animal() ?></li>
+        <li class="list-group-item">Price: <?php echo $kennel->getPrice() ?></li>
+        <li class="list-group-item">Size: <?php echo $kennel->getSize() ?></li>
         
       </ul>
     </p>
